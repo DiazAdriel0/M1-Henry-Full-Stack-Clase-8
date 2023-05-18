b152b1ae5a13982ae033fb5002049530598dcec7
DiazAdriel0 <diazadriel0@gmail.com>
2023-05-18 10:19:30 -0300

    Corrected homework

--- homework.js
+++ homework.js
@@ -36,68 +36,136 @@ function quickSort(array) {
 }
 console.log(quickSort([2, 3, 1, 8, 5, 22, 44, 8, 65, 12 ]));
 
-console.log(mergeSort([5, 1, 4, 2, 8, 6]));
-function mergeSort(array) {
-  // Implementar el método conocido como mergeSort para ordenar de menor a mayor
-  // el array recibido como parámetro
-  // Devolver el array ordenado resultante
-  // Tu código:
-  /*
-  Pseudocodigo:
-  1- divido el array en 2 mitades (usar math floor por si es impar la longitud) hasta que ya no se pueda dividir
-  2- caso base es cuando tengo arrays de longitud 1
-  3- fusiono ordenados los arrays de longitud 1
-  4- 
-  */ 
-  //okey probemos ahora
+//Corregido por Gama
+/* function quickSort(array){
   if(array.length <= 1){
     return array
   }
-  
-  //mergeSort(merge(dividir(array)))
-  //mergeSort(array)
-  // return mergeSort(dividir(array))
-}
-
-function dividir(array){
-  if(array.length <= 2){
-    return array
-  }
-  let leftArr = array.slice(0, Math.ceil(array.length / 2));
-  let rightArr = array.slice((Math.ceil(array.length / 2)))
-  return [leftArr,rightArr]
-}
-console.log(dividir([5]))
-console.log(dividir([5, 1]))
-console.log(dividir([5, 1, 4, 2, 8, 6]))
-console.log(dividir([5, 1, 4, 2, 8, 6]))
-
-console.log([1,4,5],[2,8,6]);
-
-function merge(array){
-  let leftArr = array[0];
-  console.log(leftArr)
-  let rightArr = array[1];
-  let combArr = []
-  console.log(rightArr)
-  while(leftArr.length !== 0 || rightArr.length !== 0){
-    if(leftArr[0] <= rightArr[0] || rightArr.length === 0){
-      combArr.push(leftArr.shift())
-      console.log(leftArr)
-      console.log(combArr)
-    }else if(leftArr[0] > rightArr[0] || leftArr.length === 0){
-      combArr.push(rightArr.shift())
-      console.log(rightArr)
-      console.log(combArr)
+  let pivot = array[Math.floor(Math.random() * array.length)]
+  let izq = []
+  let der = []
+  let equal = []
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < pivot){
+      izq.push(array[i])
+    }else if(array[i] > pivot){
+      der.push(array[i])
+    }else{
+      equal.push(array[i])
     }
   }
-  return combArr
+  return quickSort(izq).concat(equal).concat(quickSort(der))
+} */
+
+
+
+console.log(mergeSort([5, 1, 4, 2, 8, 6]));
+function mergeSort(array) {
+  function merge(leftArray, rightArray) {
+    const mergedArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    // Comparar los elementos de las dos mitades y agregarlos al array combinado en orden
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+        mergedArray.push(leftArray[leftIndex]);
+        leftIndex++;
+      } else {
+        mergedArray.push(rightArray[rightIndex]);
+        rightIndex++;
+      }
+    }
+  
+    // Agregar los elementos restantes de la mitad izquierda
+    while (leftIndex < leftArray.length) {
+      mergedArray.push(leftArray[leftIndex]);
+      leftIndex++;
+    }
+  
+    // Agregar los elementos restantes de la mitad derecha
+    while (rightIndex < rightArray.length) {
+      mergedArray.push(rightArray[rightIndex]);
+      rightIndex++;
+    }
+    console.log(mergedArray);
+    return mergedArray;
+  }
+  if (array.length <= 1) {
+    return array;
+  }
+
+  // Dividir el array en mitades
+  const mid = Math.floor(array.length / 2);
+  const leftArray = array.slice(0, mid);
+  const rightArray = array.slice(mid);
+
+  // Recursivamente ordenar las mitades
+  const sortedLeftArray = mergeSort(leftArray);
+  const sortedRightArray = mergeSort(rightArray);
+
+  // Combinar las mitades ordenadas
+  return merge(sortedLeftArray, sortedRightArray);
 }
 //console.log(merge(dividir([2, 1])))
-console.log(merge([[1],[2]]))
-console.log(merge([[1,4],[2,6]]))
-console.log(merge([[1,4,5],[2,6,8]]))
-console.log(merge(dividir([5, 1, 4, 2, 8, 6])))
+// console.log(merge([[1],[2]]))
+// console.log(merge([[1,4],[2,6]]))
+// console.log(merge([[1,4,5],[2,6,8]]))
+console.log(mergeSort([5, 1, 4, 2, 8, 6]))
+
+
+//Corregido por Gama
+/* function mergeSort(array){
+  function dividir(array){
+    let medio = Math.floor(array.length/2)
+    let izq = array.slice(0,medio)
+    let der = array.slice(medio)
+    return [izq,der]
+  }
+  function merge(izq,der){
+    let indiceIzq = 0
+    let indiceDer = 0
+    let array = []
+    while(indiceIzq < izq.length && indiceDer < der.length){
+      if(izq[indiceIzq] < der[indiceDer]){
+        array.push(izq[indiceIzq])
+        indiceIzq++
+      }else{
+        array.push(der[indiceDer])
+        indiceDer++
+      }
+    }
+    return array.concat(izq.slice(indiceIzq)).concat(der.slice(indiceDer))
+  }
+  if(array.length <= 1){
+    return array
+  }
+  let div = dividir(array) //devuelve un array posicion 0 lado izq posicion 1 lado der
+  let izq = div[0]
+  let der = div[1]
+
+  return merge(mergeSort(izq),mergeSort(der));
+} */
+//Otra version del merge
+/* function merge(array){
+  let unidos = []
+  while(izq.length && der.length){
+    if(izq[0] <= der[0]){
+      unidos.push(izq.shift())
+    }else{
+      unidos.push(der.shift())
+    }
+  }
+  while(izq.length){
+    unidos.push(izq.shift())
+  }
+  while (der.length){
+    unidos.push(der.shift())
+  }
+  return unidos
+} */
+console.log(mergeSort([5, 1, 4, 2, 8, 6]))
+
 // No modificar nada debajo de esta línea
 // --------------------------------
 
--- pruebas.js
+++ pruebas.js
@@ -496,4 +496,62 @@ function mergeSort(array) {
 }
 
 
+//Mi intento
+// function mergeSort(array) {
+//   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
+//   // el array recibido como parámetro
+//   // Devolver el array ordenado resultante
+//   // Tu código:
+//   /*
+//   Pseudocodigo:
+//   1- divido el array en 2 mitades (usar math floor por si es impar la longitud) hasta que ya no se pueda dividir
+//   2- caso base es cuando tengo arrays de longitud 1
+//   3- fusiono ordenados los arrays de longitud 1
+//   4- 
+//   */ 
+//   //okey probemos ahora
+//   if(array.length <= 1){
+//     return array
+//   }
+  
+//   //mergeSort(merge(dividir(array)))
+//   //mergeSort(array)
+//   // return mergeSort(dividir(array))
+// }
+
+// function dividir(array){
+//   if(array.length <= 2){
+//     return array
+//   }
+//   let leftArr = array.slice(0, Math.ceil(array.length / 2));
+//   let rightArr = array.slice((Math.ceil(array.length / 2)))
+//   return [leftArr,rightArr]
+// }
+// console.log(dividir([5]))
+// console.log(dividir([5, 1]))
+// console.log(dividir([5, 1, 4, 2, 8, 6]))
+// console.log(dividir([5, 1, 4, 2, 8, 6]))
+
+// console.log([1,4,5],[2,8,6]);
+
+// function merge(array){
+//   let leftArr = array[0];
+//   console.log(leftArr)
+//   let rightArr = array[1];
+//   let combArr = []
+//   console.log(rightArr)
+//   while(leftArr.length !== 0 || rightArr.length !== 0){
+//     if(leftArr[0] <= rightArr[0] || rightArr.length === 0){
+//       combArr.push(leftArr.shift())
+//       console.log(leftArr)
+//       console.log(combArr)
+//     }else if(leftArr[0] > rightArr[0] || leftArr.length === 0){
+//       combArr.push(rightArr.shift())
+//       console.log(rightArr)
+//       console.log(combArr)
+//     }
+//   }
+//   return combArr
+// }
+
 console.log(mergeSort([5, 1, 4, 3, 9, 2, 8, 6]));
